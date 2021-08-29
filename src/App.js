40b99a42745b399cd3010964b09dbf627ac824e9
Antony Gaetano <86926752+AntonyGaetano@users.noreply.github.com>
2021-08-29 15:52:03 -0300

    Criação dos componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import RecebeDados from './RecebeDados';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  var num1 = 1;
+  var num2 = -5;
+  var num3 = 4;
+ return(
+ <>
+  <RecebeDados letraA={num1} letraB={num2} letraC={num3}/>
+  
+ </>
+ );
 }
 
 export default App;
---
+++ src/RecebeDados.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function RecebeDados(props){
+ var A;
+ var B;
+ var C;
+
+ A = props.letraA;
+ B = props.letraB;
+ C = props.letraC;
+
+  return(<div>
+   
+
+  </div>);
+}
+
